982cb79a651dada000d990bb587ca28c85d7a086
Juan Francisco Cardona Mc'Cormick <jfcmacro@gmail.com>
2019-08-23 11:47:54 -0500

    Addicion de problema de sincronizacion

---
+++ crearHilo3.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <pthread.h>
+#include <errno.h>
+#include <cstdlib>
+#include <cstring>
+#include <unistd.h>
+
+using namespace std;
+
+void* hilo(void*);
+
+const int N = 5;
+int var = 0;
+
+int
+main() {
+  pthread_t ph_hilo[N];
+
+  for (int i = 0; i < N; i++) {
+    if (pthread_create(&ph_hilo[i], nullptr, hilo, nullptr) != 0) {
+      cerr << "No se pudo crear hilo: " << errno << endl;
+      _exit(1);
+    }
+  }
+
+  for (int i = 0; i < N; i++) {
+    void *valRet;
+    pthread_join(ph_hilo[i], &valRet);
+  }
+
+  cout << "Var: " << var << endl;
+
+  return 0;
+}
+
+void* hilo(void *arg) {
+  int value = 10;
+
+  for (int i = 0; i < 5000; i++)
+    var++;
+
+  return &value;
+}
--- makefile
+++ makefile
@@ -1,7 +1,10 @@
-all: crearHilo crearHilo2
+all: crearHilo crearHilo2 crearHilo3
 
 crearHilo: crearHilo.o
 	$(CXX) -o $@ $^ -pthread
 
-crearHilo2:crearHilo2.o
+crearHilo2: crearHilo2.o
+	$(CXX) -o $@ $^ -pthread
+
+crearHilo3: crearHilo3.o
 	$(CXX) -o $@ $^ -pthread
